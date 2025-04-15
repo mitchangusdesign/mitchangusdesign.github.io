1b7e5d72191306f00cf6bdcbca56fbad702c5711
mitchangusdesign <147563844+mitchangusdesign@users.noreply.github.com>
2025-04-14 20:23:18 -0700

    Update menuComponent.js

--- static-test/menuComponent.js
+++ static-test/menuComponent.js
@@ -8,13 +8,40 @@ async function populate() {
   const menuItemsText = await response.text();
 
   const menuItems = JSON.parse(menuItemsText);
-  populateMenu(menuItems);
+  populateDrinks(menuItems);
+  populateFood(menuItems);
 
 }
      
-function populateMenu(obj) {
+function populateDrinks(obj) {
   const menu = document.querySelector('.menulist');
   const items = obj.cocktails;
+
+  for ( const item of items ) {
+    const row = document.createElement('div');
+          row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+          name.classList.add('name');
+    const desc = document.createElement('p');
+          desc.classList.add('description');
+    const price = document.createElement('p');
+          price.classList.add('price');
+      
+    name.textContent = item.name;
+    desc.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(desc);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+  }
+}
+
+function populateFood(obj) {
+  const menu = document.querySelector('.menulistfood');
+  const items = obj.dinners;
 
   for ( const item of items ) {
     const row = document.createElement('div');
