6b89975b102dae8c1f75c1a3a49f666864f5b080
mitchangusdesign <147563844+mitchangusdesign@users.noreply.github.com>
2025-04-14 17:21:28 -0700

    Update menuComponent.js

--- static-test/menuComponent.js
+++ static-test/menuComponent.js
@@ -1,40 +1,39 @@
 async function populate() {
+
   const requestURL = "https://github.com/mitchangusdesign/mitchangusdesign.github.io/blob/main/static-test/items.json";
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  
   const menuItemsText = await response.text();
 
   const menuItems = JSON.parse(menuItemsText);
-  
-      populateMenu(menuItems);
-  
-     }
-     
-     
-      function populateMenu(obj) {
-      const menu = 
-document.querySelector('menu-items')
-      const items = obj.cocktails;
-      
-      
+  populateMenu(menuItems);
 
-      for ( const item of items ) {
-      const item = document.createElement('div');
-        div.classList.add('item-wrapper');
-      const name = document.createElement('h3');
-        div.classList.add('name');
-      const desc = document.createElement('p');
-        div.classList.add('description');
-      const price = document.createElement('p');
-        div.classList.add('price');
-        
-        
-        name.textContent = cocktails.name;
-        desc.textContent = cocktails.description;
-        price.textContent = `$ ${cocktails.price}`;
-      };
-     
 }
-    populate();
+     
+function populateMenu(obj) {
+  const menu = document.getElementById('menulist');
+  const items = obj.cocktails;
+
+  for ( const item of items ) {
+    const row = document.createElement('div');
+          row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+          name.classList.add('name');
+    const desc = document.createElement('p');
+          desc.classList.add('description');
+    const price = document.createElement('p');
+          price.classList.add('price');
+      
+    name.textContent = item.name;
+    desc.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(desc);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+  }
+}
+populate();
