e57f56393f93ee9c76e5118c50254308c6fadc78
mitchangusdesign <147563844+mitchangusdesign@users.noreply.github.com>
2025-04-21 10:02:14 -0700

    Update supabaseComponent.js

--- static-test/supabaseComponent.js
+++ static-test/supabaseComponent.js
@@ -43,151 +43,151 @@ async function populateApps()  {
 };
 
 // Lunch Menu
-    async function populateLunch()  {
-          const menu = document.querySelector('.menulistLunch');
-          const { data: items } = await supabasePublicClient
-          .from('menu')
-          .select('id, name, description, price')
-          .eq('category', 'lunch')
-          .order('id', { ascending: true });
-      
-          if (!items) {
-          return console.error('No items found');
-          }
-      
-          console.log(items);
-         
-          for ( const item of items ) {
-          const row = document.createElement('div');
-                row.classList.add('item-wrapper');
-          const name = document.createElement('h3');
-                name.classList.add('name');
-          const description = document.createElement('p');
-                description.classList.add('description');
-          const price = document.createElement('p');
-                price.classList.add('price');
-                
-          name.textContent = item.name;
-          description.textContent = item.description;
-          price.textContent = item.price;
+async function populateLunch()  {
+    const menu = document.querySelector('.menulistLunch');
+    const { data: items } = await supabasePublicClient
+    .from('menu')
+    .select('id, name, description, price')
+    .eq('category', 'lunch')
+    .order('id', { ascending: true });
 
-          row.appendChild(name); 
-          row.appendChild(description);
-          row.appendChild(price); 
-                
-          menu.appendChild(row);      
-          };
-   };
+    if (!items) {
+    return console.error('No items found');
+    }
+
+    console.log(items);
+    
+    for ( const item of items ) {
+    const row = document.createElement('div');
+        row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+        name.classList.add('name');
+    const description = document.createElement('p');
+        description.classList.add('description');
+    const price = document.createElement('p');
+        price.classList.add('price');
+        
+    name.textContent = item.name;
+    description.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(description);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+    };
+};
 
 // Dinner Menu
-    async function populateDinner()  {
-          const menu = document.querySelector('.menulistDinner');
-          const { data: items } = await supabasePublicClient
-          .from('menu')
-          .select('id, name, description, price')
-          .eq('category', 'dinner')
-          .order('id', { ascending: true });
-      
-          if (!items) {
-          return console.error('No items found');
-          }
-      
-          console.log(items);
-         
-          for ( const item of items ) {
-          const row = document.createElement('div');
-                row.classList.add('item-wrapper');
-          const name = document.createElement('h3');
-                name.classList.add('name');
-          const description = document.createElement('p');
-                description.classList.add('description');
-          const price = document.createElement('p');
-                price.classList.add('price');
-                
-          name.textContent = item.name;
-          description.textContent = item.description;
-          price.textContent = item.price;
+async function populateDinner()  {
+    const menu = document.querySelector('.menulistDinner');
+    const { data: items } = await supabasePublicClient
+    .from('menu')
+    .select('id, name, description, price')
+    .eq('category', 'dinner')
+    .order('id', { ascending: true });
 
-          row.appendChild(name); 
-          row.appendChild(description);
-          row.appendChild(price); 
-                
-          menu.appendChild(row);      
-          };
-   };
+    if (!items) {
+    return console.error('No items found');
+    }
+
+    console.log(items);
+    
+    for ( const item of items ) {
+    const row = document.createElement('div');
+        row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+        name.classList.add('name');
+    const description = document.createElement('p');
+        description.classList.add('description');
+    const price = document.createElement('p');
+        price.classList.add('price');
+        
+    name.textContent = item.name;
+    description.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(description);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+    };
+};
 // Soft Drinks Menu
-    async function populateSodas()  {
-          const menu = document.querySelector('.menulistSodas');
-          const { data: items } = await supabasePublicClient
-          .from('menu')
-          .select('id, name, description, price')
-          .eq('category', 'softdrinks')
-          .order('id', { ascending: true });
-      
-          if (!items) {
-          return console.error('No items found');
-          }
-      
-          console.log(items);
-         
-          for ( const item of items ) {
-          const row = document.createElement('div');
-                row.classList.add('item-wrapper');
-          const name = document.createElement('h3');
-                name.classList.add('name');
-          const description = document.createElement('p');
-                description.classList.add('description');
-          const price = document.createElement('p');
-                price.classList.add('price');
-                
-          name.textContent = item.name;
-          description.textContent = item.description;
-          price.textContent = item.price;
+async function populateSodas()  {
+    const menu = document.querySelector('.menulistSodas');
+    const { data: items } = await supabasePublicClient
+    .from('menu')
+    .select('id, name, description, price')
+    .eq('category', 'softdrinks')
+    .order('id', { ascending: true });
 
-          row.appendChild(name); 
-          row.appendChild(description);
-          row.appendChild(price); 
-                
-          menu.appendChild(row);      
-          };
-   };
+    if (!items) {
+    return console.error('No items found');
+    }
+
+    console.log(items);
+    
+    for ( const item of items ) {
+    const row = document.createElement('div');
+        row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+        name.classList.add('name');
+    const description = document.createElement('p');
+        description.classList.add('description');
+    const price = document.createElement('p');
+        price.classList.add('price');
+        
+    name.textContent = item.name;
+    description.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(description);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+    };
+};
 
    //Cocktail Menu
-    async function populateCocktails() {
-          const menu = document.querySelector('.menulistCocktails');
-          const { data: items } = await supabasePublicClient
-          .from('menu')
-          .select('id, name, description, price')
-          .eq('category', 'cocktails')
-          .order('id', { ascending: true });
-      
-          if (!items) {
-          return console.error('No items found');
-          }
-      
-          console.log(items);
-         
-          for ( const item of items ) {
-          const row = document.createElement('div');
-                row.classList.add('item-wrapper');
-          const name = document.createElement('h3');
-                name.classList.add('name');
-          const description = document.createElement('p');
-                description.classList.add('description');
-          const price = document.createElement('p');
-                price.classList.add('price');
-                
-          name.textContent = item.name;
-          description.textContent = item.description;
-          price.textContent = item.price;
+async function populateCocktails() {
+    const menu = document.querySelector('.menulistCocktails');
+    const { data: items } = await supabasePublicClient
+    .from('menu')
+    .select('id, name, description, price')
+    .eq('category', 'cocktails')
+    .order('id', { ascending: true });
 
-          row.appendChild(name); 
-          row.appendChild(description);
-          row.appendChild(price); 
-                
-          menu.appendChild(row);      
-          }
-   };
+    if (!items) {
+    return console.error('No items found');
+    }
+
+    console.log(items);
+    
+    for ( const item of items ) {
+    const row = document.createElement('div');
+        row.classList.add('item-wrapper');
+    const name = document.createElement('h3');
+        name.classList.add('name');
+    const description = document.createElement('p');
+        description.classList.add('description');
+    const price = document.createElement('p');
+        price.classList.add('price');
+        
+    name.textContent = item.name;
+    description.textContent = item.description;
+    price.textContent = item.price;
+
+    row.appendChild(name); 
+    row.appendChild(description);
+    row.appendChild(price); 
+        
+    menu.appendChild(row);      
+    };
+};
 
 populateApps();
 populateLunch();
