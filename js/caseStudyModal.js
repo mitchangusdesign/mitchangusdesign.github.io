d01f28084d940823a24d73e694547aa66855d3c2
mitchangusdesign <147563844+mitchangusdesign@users.noreply.github.com>
2026-08-15 20:03:18 -0700

    re engineered modal behavior

--- js/caseStudyModal.js
+++ js/caseStudyModal.js
@@ -1,35 +1,35 @@
 function openModal() {
-      document.getElementById("myModal").style.display = "flex";
-    }
-    
-    function closeModal() {
-      document.getElementById("myModal").style.display = "none";
-    }
+  document.getElementById("myModal").showModal();
+}
 
-    function openToSlide(x) {
-    var element = document.getElementById(x)
-        element.scrollIntoView({ behavior: 'instant' })
-    }
+function closeModal() {
+  document.getElementById("myModal").close();
+}
 
-    // Modal Nav Button Horizontal Scroll Mechanism
-  
-    // Take the value of the left or right button and multiply by the width of container and scroll page by the result 
-    function scrollNext(x, y) {
-      const container = document.getElementById('scroll-wrap');
-      const slideWidth = container.clientWidth;
-      container.scrollBy({
-      top: y,
-      left:slideWidth * x,
-      behavior: "smooth",
-    });
-    }
+function openToSlide(x) {
+var element = document.getElementById(x)
+    element.scrollIntoView({ behavior: 'instant' })
+}
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
+// Modal Nav Button Horizontal Scroll Mechanism
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+// Take the value of the left or right button and multiply by the width of container and scroll page by the result 
+function scrollNext(x, y) {
+  const container = document.getElementById('scroll-wrap');
+  const slideWidth = container.clientWidth;
+  container.scrollBy({
+  top: y,
+  left:slideWidth * x,
+  behavior: "smooth",
+});
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.close();
+  }
+}
